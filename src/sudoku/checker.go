3354d98b75877a6b00dea855ac39a800a894fc71
Siwei Xu <xusiwei1236@163.com>
2017-02-26 18:41:43 +0800

    add SudokuChecker and it's unit test

---
+++ src/sudoku/checker.go
@@ -0,0 +1,78 @@
+package sudoku
+
+const (
+	Boxs      = 9
+	BoxRows   = 3
+	BoxCols   = 3
+	BoxCells  = BoxRows * BoxCols
+	MinValue  = 0
+	MaxValue  = 9
+	NumValues = MaxValue - MinValue + 1
+)
+
+/*
+type Checker interface {
+	Check(p *Puzzle) bool
+}
+*/
+
+type SudokuChecker struct {
+}
+
+// constructor
+func NewChecker() *SudokuChecker {
+	return &SudokuChecker{}
+}
+
+// methods
+func (checker *SudokuChecker) Check(p *Puzzle) bool {
+	// check cell values
+	for i := 0; i < Rows; i++ {
+		for j := 0; j < Cols; j++ {
+			v := p.Get(i, j)
+			if v < 0 || v > 9 {
+				return false
+			}
+		}
+	}
+
+	// check each row
+	for i := 0; i < Rows; i++ {
+		count := [NumValues]int{}
+		for j := 0; j < Cols; j++ {
+			v := p.Get(i, j)
+			if count[v] > 1 {
+				return false
+			}
+			count[v] += 1
+		}
+	}
+
+	// check each cols
+	for j := 0; j < Cols; j++ {
+		count := [NumValues]int{}
+		for i := 0; i < Rows; i++ {
+			v := p.Get(i, j)
+			if count[v] > 1 {
+				return false
+			}
+			count[v] += 1
+		}
+	}
+
+	// check each box
+	for b := 0; b < Boxs; b++ {
+		br := b / (Cols / BoxCols) * BoxCols
+		bc := b % (Cols / BoxCols) * BoxCols
+		count := [NumValues]int{}
+		for i := 0; i < BoxCells; i++ {
+			v := p.Get(br+i/BoxCols, bc+i%BoxCols)
+			if count[v] > 1 {
+				return false
+			}
+			count[v] += 1
+
+		}
+	}
+	return true
+}
--- src/test.sudoku.go
+++ src/test.sudoku.go
@@ -13,6 +13,47 @@ func assert(expr bool, text string) {
 func main() {
 	defer fmt.Println("done")
 
+	puzzleTest()
+	checkerTest()
+}
+
+func checkerTest() {
+	c := sudoku.NewChecker()
+
+	checkPuzzle := func(puz string, expected bool) {
+		p := sudoku.NewPuzzle(puz)
+
+		printPuzzle(p)
+		fmt.Printf("puzzle: %s\n", puz)
+		fmt.Println("valid:", c.Check(p))
+
+		assert(c.Check(p) == expected, "Checker.Check() assertion failed")
+	}
+
+	checkPuzzle(func() string {
+		puz := ""
+		for i := 0; i < sudoku.Cells; i++ {
+			puz += strconv.Itoa(i % 9)
+		}
+		return puz
+	}(), false)
+
+	checkPuzzle(func() string {
+		puz := ""
+		puz += "268597134"
+		puz += "439618527"
+		puz += "571423896"
+		puz += "715239468"
+		puz += "942856371"
+		puz += "386174259"
+		puz += "894365712"
+		puz += "623741985"
+		puz += "157982643"
+		return puz
+	}(), true)
+}
+
+func puzzleTest() {
 	puzStr := ""
 	for i := 0; i < sudoku.Cells; i++ {
 		puzStr += strconv.Itoa(i % 9)
@@ -38,11 +79,10 @@ func main() {
 				fmt.Sprintf("assertion failed on (%d, %d)", i, j))
 		}
 	}
-
 }
 
 func printPuzzle(p *sudoku.Puzzle) {
-	fmt.Printf("|")
+	fmt.Printf("\n|")
 	for j := 0; j < sudoku.Cols; j++ {
 		fmt.Printf(" %d |", p.Get(0, j))
 	}
