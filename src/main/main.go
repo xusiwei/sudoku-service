ccd0d47f49bf6e44e20fc7b1cbac6f2ddab28cc5
Siwei Xu <xusiwei1236@163.com>
2017-03-02 22:21:04 +0800

    add command option support

--- src/main/main.go
+++ src/main/main.go
@@ -1,14 +1,33 @@
 package main
 
+import "os"
+import "fmt"
+import "log"
+import "flag"
+import "errors"
 import "net/http"
 import "service"
 
+var port = flag.Int("port", 9988, "The port you want to bind")
+var logfile = flag.String("log", "run.log", "Log file write back")
+
+var ErrHelp = errors.New("flag: help requested")
+
 func main() {
+
+	flag.Parse()
+
+	address := fmt.Sprintf("127.0.0.1:%d", *port)
+	if fp, err := os.Create(*logfile); err == nil {
+		fmt.Println("loging to file", *logfile)
+		log.SetOutput(fp)
+	}
+	fmt.Println("listen on address", address)
 
 	mux := http.NewServeMux()
 	mux.Handle("/puzzles", service.NewPuzzleService())
 	mux.Handle("/checker", service.NewCheckerService())
 	mux.Handle("/solver", service.NewSolverService())
 
-	http.ListenAndServe("127.0.0.1:9988", mux)
+	http.ListenAndServe(address, mux)
 }
