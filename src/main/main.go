166d87e5600488366fde59534524c22d4abdf78a
Siwei Xu <xusiwei1236@163.com>
2017-03-15 19:43:31 +0800

    [main] add graceful shutdown support

--- src/main/main.go
+++ src/main/main.go
@@ -5,12 +5,15 @@ import "fmt"
 import "log"
 import "flag"
 import "errors"
+import "context"
 import "net/http"
+import "syscall"
+import "os/signal"
 import "service"
 
 var port = flag.Int("port", 9988, "The port you want to bind")
 var host = flag.String("host", "localhost", "The host you want to bind")
-var logfile = flag.String("log", "run.log", "Log file write back")
+var logfile = flag.String("log", "", "Log file write back")
 
 var ErrHelp = errors.New("flag: help requested")
 
@@ -31,5 +34,33 @@ func main() {
 	mux.Handle("/checker", service.NewCheckerService())
 	mux.Handle("/solver", service.NewSolverService())
 
-	http.ListenAndServe(address, mux)
+	server := http.Server{
+		Addr:    address,
+		Handler: mux,
+	}
+
+	done := make(chan bool, 1)
+
+	go func() {
+		server.ListenAndServe()
+		done <- true
+	}()
+	log.Printf("server goroutine started.")
+
+	go func() {
+		ctx := context.TODO()
+		sch := make(chan os.Signal, 1)
+		signal.Notify(sch, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGINT)
+		select {
+		case s := <-sch:
+			log.Printf("signal raised: %v", s)
+			server.Shutdown(ctx) // go1.8
+		}
+	}()
+	log.Printf("signal monitor goroutine started.")
+
+	log.Printf("waiting for server done...")
+	<-done
+
+	log.Printf("server done")
 }
